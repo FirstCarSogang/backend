c2e2c87a32fb65b0e1d6f30100954aef9dc63db7
acupoframen <choe.hwan@gmail.com>
2024-02-02 13:55:31 +0900

    2024_02_02 1:55 pm_hwan commit changed login, login is fuctioning now.

--- firstCarSogang_backend/firstCarSogang_signuplogin/forms.py
+++ firstCarSogang_backend/firstCarSogang_signuplogin/forms.py
@@ -10,24 +10,18 @@ class EmailAuthenticationForm(AuthenticationForm):
 
 class UserForm(UserCreationForm):
     email = forms.EmailField(label="이메일")
-
     class Meta:
         model = NormalUser
         fields = ['username', 'name', 'email', 'kakaotalkID', 'password1', 'photo1', 'photo2', 'photo3']
-
     def clean_studentID(self):
         student_id = self.cleaned_data['username']
         if len(str(student_id)) != 8:
             raise ValidationError("Student ID must be 8 digits long.")
-
         if NormalUser.objects.filter(username=student_id).exists():
             raise ValidationError("This student ID is already in use.")
-
         return student_id
-
     def clean_kakaotalkID(self):
         kakaotalk_id = self.cleaned_data['kakaotalkID']
         if NormalUser.objects.filter(kakaotalkID=kakaotalk_id).exists():
             raise ValidationError("This KakaoTalk ID is already in use.")
-
         return kakaotalk_id
